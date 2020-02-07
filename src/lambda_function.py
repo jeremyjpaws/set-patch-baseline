2cd47a03774563f1477fe8e0174999cf089c50c7
EC2 Default User <ec2-user@ip-172-64-1-199.ec2.internal>
2020-02-07 19:28:34 +0000

    working Lambda function that sets a given baseline to default. Next TODO working with CloudFormation

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -12,17 +12,24 @@ def set_baseline(mybaseline):
 
     ssm = boto3.client('ssm')
 
+    logger.info(f'About to set baseline {mybaseline} as the default...')
+
     response = ssm.register_default_patch_baseline(
         BaselineId=mybaseline
     )
 
+    logger.info(f'SSM response is {response}')
+
 def lambda_handler(event, context):
     logger.info('Beginning function...')
-    logger.info('got event{}'.format(event))
+    logger.info(f'got event: {event}')
     
     # Get from event data the baseline we want to set
+    baselineId = event["baseline"]
+    logger.info(f'Baseline ID is: {baselineId}')
 
     # TODO Now set our baseline
+    set_baseline(baselineId)
 
     # TODO And signal back
 
