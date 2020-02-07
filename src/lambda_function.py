b8714a6113561a02e11d6154573d09fe5039d87f
jeremyjpaws <ec2-user@ip-172-64-1-199.ec2.internal>
2020-02-07 20:29:29 +0000

    added cfn code and some error handling

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,6 +5,7 @@ import json
 import boto3
 import logging
 import os
+import cfnresponse
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -20,20 +21,47 @@ def set_baseline(mybaseline):
 
     logger.info(f'SSM response is {response}')
 
+def signal_success():
+    logger.info('Sending success response back to CloudFormation')
+    responseData = {}
+    responseData['Comment'] = 'example custom respone data where we could return custom attributes'
+    cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+
+def signal_failure():
+    logger.info('Sending FAILURE response back to CloudFormation')
+    responseData = {}
+    responseData['Comment'] = 'example custom respone data where we could return custom attributes'
+    cfnresponse.send(event, context, cfnresponse.FAILURE, responseData)
+
 def lambda_handler(event, context):
     logger.info('Beginning function...')
-    logger.info(f'got event: {event}')
+    logger.info(f'Got event: {event}')
     
-    # Get from event data the baseline we want to set
-    baselineId = event["baseline"]
+    requestType = event['requestType']
+    baselineId = event['ResourceProperties']['baselineId']
+    logger.info(f'Request Type is: {requestType}')
     logger.info(f'Baseline ID is: {baselineId}')
 
-    # TODO Now set our baseline
-    set_baseline(baselineId)
-
-    # TODO And signal back
+    # Now set our baseline
+    if requestType == "Create" or requestType == "Update":
+        logger.info(f'Got a {requestType} request, setting default baseline...')
+        try:
+            set_baseline(baselineId)
+            signal_success()
+        except:
+            logger.error(f'Unknown failure in setting default patch baseline!')
+            signal_failure()
+    else if requestType == "Delete":
+        #  No good definition for "undoing" a default set
+        #  TODO One option here would be to add code to set default back to the AWS Default
+        logger.info('Got Delete request, doing nothing except signalling back success.')
+        signal_success()
+    else:
+        logger.info(f'Request type is not recognized! Request Type was {requestType}')
+        
 
     return {
         'statusCode': 200,
         'body': json.dumps('Lambda function ending.')
     }
+
